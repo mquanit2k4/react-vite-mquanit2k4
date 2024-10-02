ec25e268f934d24cb420751add96cdf0364afa74
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-10-02 17:58:49 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,23 @@
 // () => {  } is a function component
+// JSX
+// Using fragment <> </> to wrap multiple elements in a single parent element
+// With JSX file, we change class to className, for, to htmlFor, onclick to onClick, tabindex to tabIndex, and so on
+import "./style.css";
 
 const MyComponent = () => {
-    return (
-      <div> Vu Minh Quan SoICT-HUST </div>
-    );
-}
+  return (
+    <>
+      <div> Vu Minh Quan SoICT-HUST </div> //tag
+      <div
+        className="child"
+        style={{
+          borderRadius: "10px",
+        }}
+      >
+        child
+      </div>
+    </>
+  );
+};
 
 export default MyComponent;
