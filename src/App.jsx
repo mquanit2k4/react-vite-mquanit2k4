0d54788b3c27e3da167ef4a604dd0ad79e9fc203
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-10-03 18:00:16 +0700

    video 35

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import TodoNew from "./components/todo/TodoNew";
 import TodoData from "./components/todo/TodoData";
 import reactLogo from "./assets/react.svg";
 const App = () => {
-  const QuanVM = "Vu Minh Quan"; 
+  const QuanVM = "Vu Minh Quan K67"; 
   const age = 20;
   const data = {
     name: "Vu Minh Quan",
     age: 20,
   };
+
+  const addNewTodo = (name) => {
+    alert(`>> Call me ${name}`);
+  };
+
   return (
     <div className="todo-container">
       <div className="todo-title"> Todo List</div>
-      <TodoNew />
+      <TodoNew
+      addNewTodo = {addNewTodo} // Truyen khac thuc thi 
+      />
       <TodoData 
       name = {QuanVM}
       age = {age}
       data = {data}
+      
       />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,5 +1,8 @@
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(">>> check props", props);
+    const {addNewTodo} = props;
+    addNewTodo('Vu Minh Quan');
     return (
         <div className="todo-new">
         <input type="text" className="todo-input" />
