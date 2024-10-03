9debe7e7826abc34536c30ab0cd479207d8d42a1
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-10-03 18:16:49 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,14 +1,24 @@
-
 const TodoNew = (props) => {
-    console.log(">>> check props", props);
-    const {addNewTodo} = props;
-    addNewTodo('Vu Minh Quan');
-    return (
-        <div className="todo-new">
-        <input type="text" className="todo-input" />
-        <button className="todo-button">Add</button>
-      </div>
-    )
-}
+  console.log(">>> check props", props);
+  // const { addNewTodo } = props;
+  // addNewTodo("Vu Minh Quan");
+
+  const handleClick = () => {
+    alert("Click me");
+  };
+
+  const handlOnChange = (name) => {
+    console.log(">>> check input", name);
+  }
+  return (
+    <div className="todo-new">
+      <input type="text"
+      onChange={(event) => handlOnChange(event.target.value)} className="todo-input" />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
+    </div>
+  );
+};
 
 export default TodoNew;
