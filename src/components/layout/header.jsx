dc8f1a278867dd7f6c9a2c25307dff738c22c6dc
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-10-05 23:03:49 +0700

    video 49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,20 +1,21 @@
 import "./header.css";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <ul>
       <li>
-        <a className="active" href="/">
+        <Link className="active" to="/">
           Home
-        </a>
+        </Link>
       </li>
       <li>
-        <a href="/users">Users</a>
+        <Link to="/users">Users</Link>
       </li>
       <li>
-        <a href="/products">Products</a>
+        <Link to="/products">Products</Link>
       </li>
       <li>
-        <a href="/register">Register</a>
+        <Link to="/register">Register</Link>
       </li>
     </ul>
   );
