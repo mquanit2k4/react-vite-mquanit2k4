2fbbe149067f516a1607d27040a8147278966666
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-10-06 08:29:28 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,20 @@ import App from './App.jsx'
 import LoginPage from './pages/login.jsx'
 import RegisterPage from './pages/register.jsx'
 import UserPage from './pages/user.jsx'
-import ProductPage from './pages/product.jsx'
+import BookPage from './pages/book.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./styles/global.css";
 import TodoApp from './components/todo/TodoApp.jsx'
+import ErrorPage from './pages/error.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -26,8 +28,8 @@ const router = createBrowserRouter([
         element: <UserPage/>,
       },
       {
-        path: "/products",
-        element: <ProductPage/>,
+        path: "/books",
+        element: <BookPage/>,
       }
     ]
   },
